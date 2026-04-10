da93ef9f3c50de450587994bae89f74eb1305ca8
Mikołaj <mikolaj.moozoluk@gmail.com>
2026-04-11 01:30:09 +0200

    dodalem druga instancje backendu i nadalem im zmienna srodowiskowa instance_id

--- Zadanie3v2/scripts/04-run.sh
+++ Zadanie3v2/scripts/04-run.sh
@@ -2,8 +2,15 @@
 set -e
 
 docker run -d \
-  --name product-dashboard-backend \
+  --name api-a \
   --network product-dashboard-net \
+  -e INSTANCE_ID=api-a \
+  ghcr.io/mozlook/product-dashboard-backend:v1
+
+docker run -d \
+  --name api-b \
+  --network product-dashboard-net \
+  -e INSTANCE_ID=api-b \
   ghcr.io/mozlook/product-dashboard-backend:v1
 
 docker run -d \
